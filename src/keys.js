642f012c68e0adcb0ef7ac34204aa038da458403
facundo <brizuelafacundoignacio@gmail.com>
2021-07-23 13:00:08 -0300

    Keys de heroku database mysql

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,14 @@
 module.exports =  {
     database: {
-        host:'localhost',
-        user:'root',
-        password:'39845733',
-        database: 'database_links'
+        host:'eu-cdbr-west-01.cleardb.com',
+        user:'b2528b48b9b425',
+        password:'b3375265',
+        database: 'heroku_54ed6c0cab01a36'
     }
+    // ,databaseLocalhost: {
+    //     host:'localhost',
+    //     user:'root',
+    //     password:'39845733',
+    //     database: 'database_links'
+    // }
 };
